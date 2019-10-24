567afc86f29c7b7e72580314e0009892ace4d692
100176905 <jacobacosta32@yahoo.com>
2019-10-24 15:27:44 -0500

    repetition

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -7,3 +7,21 @@ firstName = "Wilma"
 lastName = "Flintstone"
 
 print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# Repetition
+#   repitition operator: *
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row. "*3 + " your boat")
+    print("gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
